1f93f3cd19ada1d2ee7cef0f05dd4370fc3459db
juniordeveloper11 <89851068+juniordeveloper11@users.noreply.github.com>
2023-05-23 22:50:01 -0300

    Add files via upload

---
+++ estruturasCondicionaisSwitch.java
@@ -0,0 +1,43 @@
+package estruturasCondicionais;
+
+import java.lang.runtime.SwitchBootstraps;
+
+public class estruturasCondicionaisSwitch {
+
+	public static void main(String[] args) {
+		
+		byte dia = 5;
+		switch (dia) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda");
+			break;
+		case 3:
+			System.out.println("Terça");
+			break;
+		case 4:
+			System.out.println("Quarta");
+			break;
+		case 5:
+			System.out.println("Quinta");
+			break;
+		case 6:
+			System.out.println("Sexta");
+			break;
+		case 7:
+			System.out.println("Sabado");
+			break;
+		
+
+		
+			
+		}
+			
+		}
+		
+
+	}
+
+
